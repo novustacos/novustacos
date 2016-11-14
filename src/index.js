017154c00da21d05ba986d8f9a9f19fc062bdeea
Jenessa <jenessaw31@gmail.com>
2016-11-14 12:22:43 -0600

    working on adding slider

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ $('.showMenu-drink').click(
     	$(this).next('.menu-drink-list').toggle();
 	}
 );
+// 
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
+//
+// $('.fade').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: 'linear'
+// });
